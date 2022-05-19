9590e8d71a3b7bc0dab3943750630ec0197d3b4b
atezer <armin.tezer@gmail.com>
2022-05-19 21:42:02 +0200

    task1cd

--- EX1cd/task-1cd.py
+++ EX1cd/task-1cd.py
@@ -7,10 +7,11 @@ from matplotlib.animation import FuncAnimation
 
 from task_1b_utils import analytical_result, first_order_tridiags, thomas_algorithm, second_order_tridiags
 
-N_t = 50000 # timesteps
-N_x = 11
-dt = 0.01
-dx = 1/(N_x - 1)
+N_t = 500 # timesteps
+N_x = 50
+dt = 2*10**(-4)
+dx = 4 * 10 ** (-3)
+#dx = 1/(N_x - 1)
 D = 10**(-6)
 x = np.arange(0,1+dx,dx)
 t = np.arange(0,N_t * dt,dt)
@@ -20,6 +21,7 @@ C = np.zeros((N_t,N_x))
 C[0,0] = 1 # B.C.
 s = dt/(dx**2)
 s *= D
+print(f"s: {s}")
 a,b,c = first_order_tridiags(N_x,s)
 
 #Task 1d
@@ -27,6 +29,7 @@ C_inv = np.zeros((N_t,N_x))
 C_inv[0,0] = 1
 s_inv = (dt/(dx**2))/2
 s_inv *= D
+print(f"s_inv: {s_inv}")
 a_inv,b_inv,c_inv = second_order_tridiags(N_x,s_inv)
 
 C_an = np.zeros((N_t,N_x))
@@ -58,15 +61,6 @@ sns.scatterplot(y=x,x=C_inv[-1,:], marker = '2', color = 'red', s = 100)
 plt.legend(loc='upper left', labels=['Analytic', 'Implicit 1st order', 'Implicit 2nd order'])
 plt.title('Implicit Method for 10000 0.002 timesteps (adim)')
 plt.savefig('Implicit_Method_10000')
-#%%
-fig, ax = plt.subplots(figsize=(12,5))
-
-sns.lineplot(y=x,x=C_an[6000,:], lw=1)
-sns.scatterplot(y=x,x=C[6000,:], marker = '1', color = 'black', s = 100)
-sns.scatterplot(y=x,x=C_inv[6000,:], marker = '2', color = 'red', s = 100)
-plt.legend(loc='upper left', labels=['Analytic', 'Implicit 1st order', 'Implicit 2nd order'])
-plt.title('Implicit Method for 6000 0.002 timesteps (adim)')
-plt.savefig('Implicit_Method_6000')
 
 #%%
 fig, ax = plt.subplots(figsize=(12,5))
@@ -78,22 +72,11 @@ plt.show
 
 #%%
 fig, ax = plt.subplots(figsize=(12,5))
-sns.scatterplot(y=x,x=diff[600,:], marker='1', s=100)
-sns.scatterplot(y=x,x=diff_inv[600,:], marker='2', s=100)
-plt.legend(loc='upper left', labels=['1st order', '2nd order'])
-plt.title('Absolute Difference between Discrete and Analytic Solution of Implicit Method for 6000 0.2s timesteps')
-plt.show
-
-#%%
-fig, ax = plt.subplots(figsize=(12,5))
 sns.scatterplot(x=t,y=diff_avg[:], marker='1', s=50)
 sns.scatterplot(x=t,y=diff_inv_avg[:], marker='2', s=50)
 plt.legend(loc='upper left', labels=['1st order', '2nd order'])
 plt.title('Absolute Difference between Discrete and Analytic Solution of Implicit Method for 6000 0.2s timesteps')
 plt.show
-#%%
-ax = sns.heatmap(C[:,:], cmap = 'Greens').set(title='first order')
-# %%
 
 # %%
 
